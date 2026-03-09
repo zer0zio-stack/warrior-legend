0a8e6165d729f09210c9f2b0ab2089d1c19cebc6
zer0zio <zer0ziostack@gmail.com>
2026-03-09 20:40:43 +0800

    修复玩家受击后无法移动

--- Assets/_Game/Scripts/Player/PlayerHurtBehaviour.cs
+++ Assets/_Game/Scripts/Player/PlayerHurtBehaviour.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class HurtBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.GetComponent<PlayerController>().isAttack = true;
-    }
+    // public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // {
+    //     
+    // }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
