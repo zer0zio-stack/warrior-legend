57708d075bf99eea27941da5a3ca2beac73d570c
zer0zio <zer0ziostack@gmail.com>
2026-03-16 18:07:39 +0800

    瞪墙跳

--- Assets/_Game/Scripts/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -15,10 +15,14 @@ public class PlayerController : MonoBehaviour
 
     public bool isAttack;
 
+    public float JumpWallForce;
+
     public PhysicsMaterial2D _material2DNormal;
     public PhysicsMaterial2D _material2DWall;
     private PlayerAnimatiorController _animator;
     private CapsuleCollider2D _collider;
+
+    private bool _isJumpingToWall;
 
     private PhysicsCheck _physicsCheck;
 
@@ -76,8 +80,8 @@ public class PlayerController : MonoBehaviour
         }
 
         //滑墙：这里直接调速度，要是变摩擦力就会黏住
-        if (_physicsCheck.onWall) _rb.linearVelocityY = _rb.linearVelocityY / 2;
-
+        if (_physicsCheck.onWall) _rb.linearVelocityY = _rb.linearVelocityY / 3;
+        if (_isJumpingToWall && _rb.linearVelocityY < 0) _isJumpingToWall = false;
 
         switchMaterial();
     }
@@ -117,7 +121,7 @@ public class PlayerController : MonoBehaviour
 
     private void move()
     {
-        if (!isCrouch)
+        if (!isCrouch && !_isJumpingToWall)
             _rb.linearVelocityX = inputDirection.x * speed * Time.deltaTime;
 
 
@@ -132,6 +136,11 @@ public class PlayerController : MonoBehaviour
     private void Jump(InputAction.CallbackContext obj)
     {
         if (_physicsCheck.isGrounded) _rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        if (_physicsCheck.onWall)
+        {
+            _rb.AddForce(new Vector2(-inputDirection.x, 4.5f) * JumpWallForce, ForceMode2D.Impulse);
+            _isJumpingToWall = true;
+        }
     }
 
     public void getHurt(Transform attacker)
